ff8d9ab21b5fdd7ea8f0d352e46ad667582bbbd8
eduglezexp <eduex04@gmail.com>
2023-10-03 15:32:40 +0100

    Actualizando con los ejercicios del 2 al 9

--- Unidad-1/tareas/tarea2/src/Ejercicio3.java
+++ Unidad-1/tareas/tarea2/src/Ejercicio3.java
@@ -2,6 +2,7 @@
  * @author eduglezexp
  * Ejercicio numero 3
  */
+import java.util.Scanner;
 public class Ejercicio2 {
 
     /**
@@ -10,5 +11,18 @@ public class Ejercicio2 {
      */
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int valueN = 0;
+        int suma = 0;
+
+        while (valueN < 10) {
+            valueN = valueN + 1;
+            suma = suma + valueN;
+        }
+        System.out.println("the sum is: " + suma);
     }
 }
+
+
+
